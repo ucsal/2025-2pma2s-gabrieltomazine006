32ad58988930caa3763e5fa99dd46dd0d4fe0ebf
gabrieltomazine006 <gabriel.nascimento@ucsal.edu.br>
2025-09-01 18:51:12 -0300

    ATIVIDADE POOA SOLID - SRP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 import br.com.mariojp.solid.srp.*;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Order o = new Order();
-		o.add(new Item("Café", 8.0, 2)); // 16
-		o.add(new Item("Bolo", 12.5, 1)); // 12.5 -> subtotal 28.5
-		String receipt = new ReceiptService().generate(o);
-		System.out.println(receipt);
-		
-	}
+
+    public static void main(String[] args) {
+        System.setProperty("tax.rate", "0.08"); // 8%
+
+        Order o = new Order();
+        o.add(new Item("Café", 8.0, 2));  // 16.0
+        o.add(new Item("Bolo", 12.5, 1)); // 12.5 -> subtotal = 28.5
+
+        TaxCalculator taxcalculator = new TaxCalculator();
+        ReceiptFormatter receiptformatter = new ReceiptFormatter();
+        ReceiptService receiptService = new ReceiptService(taxcalculator, receiptformatter);
+        String receipt = receiptService.generate(o);
+        System.out.println(receipt);
+    }
 }
